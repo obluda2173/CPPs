579024a50597e8387eada6473485584af5e2c76a
obluda2173 <obluda2173@gmail.com>
2025-01-20 13:01:02 +0100

    cpp01 ex02 done

---
+++ CPP01/ex02/_main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   _main.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erian <erian@student.42>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 12:47:27 by erian             #+#    #+#             */
+/*   Updated: 2025/01/20 12:59:59 by erian            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+#define BLUE	"\e[38;5;123m"
+#define NC		"\033[0m"
+
+
+int main() {
+	std::string str = "HELLO THIS IS BRAIN";
+	std::string *strptr = &str;
+	std::string &strref = str;
+	
+	std::cout << "\nmemory:\n"
+	<< "original:  " << BLUE << &str << "\n" << NC
+	<< "pointer:   " << BLUE << &strptr << "\n" << NC
+	<< "reference: " << BLUE << &strref << "\n" << NC;
+
+	std::cout << "\nvalue:\n"
+	<< "original:  " << BLUE << str << "\n" << NC
+	<< "pointer:   " << BLUE << *strptr << "\n" << NC
+	<< "reference: " << BLUE << strref << "\n" << NC;
+
+	return 0;
+}
