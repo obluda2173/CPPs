d38f8a997436cfef8e386fd6c2d5d175fde0dfe0
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-03-21 13:48:00 +0100

    chagnes for eval

--- CPP07/ex00/src/_main.cpp
+++ CPP07/ex00/src/_main.cpp
@@ -6,7 +6,7 @@
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 13:24:03 by erian             #+#    #+#             */
-/*   Updated: 2025/03/20 12:34:06 by erian            ###   ########.fr       */
+/*   Updated: 2025/03/21 12:14:07 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ int main(void) {
 	int a = 2;
 	int b = 3;
 	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << RED << "a = " << a << NC << ", " << BLUE << "b = " << b << "\n" << NC;
+	std::cout << "min( " << RED << "a" << NC << ", " << BLUE << "b" << NC << " ) = " << ::min( a, b ) << "\n";
+	std::cout << "max( " << RED << "a" << NC << ", " << BLUE << "b" << NC << " ) = " << ::max( a, b ) << "\n";
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << RED << "c = " << c << NC << ", " << BLUE << "d = " << d << "\n" << NC;
+	std::cout << "min( " << RED << "c" << NC << ", " << BLUE << "d" << NC << " ) = " << ::min( c, d ) << "\n";
+	std::cout << "max( " << RED << "c" << NC << ", " << BLUE << "d" << NC << " ) = " << ::max( c, d ) << "\n";
 	return 0;
 }
--- CPP07/ex02/inc/Array.hpp
+++ CPP07/ex02/inc/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 17:21:42 by erian             #+#    #+#             */
-/*   Updated: 2025/03/20 14:27:34 by erian            ###   ########.fr       */
+/*   Updated: 2025/03/21 13:47:29 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,36 +21,54 @@ template<typename T>
 class Array {
 	private:
 		// attributes
-		T* _array;
+		T* _data;
 		unsigned int _size;
 
 	public:
 		// OCF
-		Array() : _array(new T), _size(0) {};
-		Array(unsigned int n) : _array(new T[n]), _size(n) {};
-		Array(const Array& other) : _array(new T[other.size()]), _size(other.size()) {
-			for (size_t i = 0; i < _size; i++)
-				_array[i] = other._array[i];
+		Array() : _data(NULL), _size(0) {}
+		Array(unsigned int n) : _size(n) {
+			_data = new T[n]();
+		}
+		Array(const Array& other) : _size(other._size) {
+			_data = new T[_size];
+			for (unsigned int i = 0; i < _size; i++)
+				_data[i] = other._data[i];
 		}
 		Array& operator=(const Array& other) {
-			delete [] _array;
-			_array = new T[other.size()];
-			_size = other.size();
-			for (size_t i = 0; i < _size; i++)
-				_array[i] = other._array[i];
+			if (this == &other)
+				return *this;
+	
+			delete[] _data;
+	
+			_size = other._size;
+			_data = new T[_size];
+			for (unsigned int i = 0; i < _size; i++)
+				_data[i] = other._data[i];
+	
 			return *this;
 		}
-		T& operator[](unsigned int index) const {
-			if (index >= _size)
-				throw std::exception();
-			return _array[index];
-		}
 		~Array() {
-			delete [] _array;
+			delete[] _data;
 		}
-
+	
+		// operators
+		T& operator[](unsigned int index) {
+			// std::out_of_range is chosen for a reason
+			if (index >= _size)
+				throw std::out_of_range("Index out of bounds");
+			return _data[index];
+		}
+		const T& operator[](unsigned int index) const {
+			// std::out_of_range is chosen for a reason
+			if (index >= _size)
+				throw std::out_of_range("Index out of bounds");
+			return _data[index];
+		}
+		
 		// methods
-		size_t size() const {
+		unsigned int size() const {
 			return _size;
 		}
 };
+	
