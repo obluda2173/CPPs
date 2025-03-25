764d53e3b4d08ed3dbbe5187a2a61612e0a3f0da
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-03-25 13:57:12 +0100

    some baseline for the exercise, main is done, general idea is catched and grasped

---
+++ CPP09/ex02/inc/PmergeMe.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 11:39:43 by erian             #+#    #+#             */
+/*   Updated: 2025/03/25 13:46:45 by erian            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <deque>
+#include <vector>
+#include <chrono>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+#include "_Colors.hpp"
+
+class PmergeMe {
+    private:
+        void mergeInsertionSort(std::vector<int> &vec);
+        void mergeInsertionSort(std::deque<int> &deq);
+    
+    public:
+        PmergeMe();
+        PmergeMe(const PmergeMe& other);
+        PmergeMe &operator=(const PmergeMe& other);
+        ~PmergeMe();
+
+        void sortAndMeasure(std::vector<int>& vec, std::deque<int>& deq);
+        static void printSequence(const std::string& msg, const std::vector<int>& vec);
+};
