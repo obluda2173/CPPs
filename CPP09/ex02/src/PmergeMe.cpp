8f0385bdb3c0444d56152cb31fb035c593bac82f
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-03-25 14:15:12 +0100

    OCF added, merge insertion algorithm implemented for both vec and deq

--- CPP09/ex02/src/PmergeMe.cpp
+++ CPP09/ex02/src/PmergeMe.cpp
@@ -6,9 +6,71 @@
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 11:40:01 by erian             #+#    #+#             */
-/*   Updated: 2025/03/25 11:43:07 by erian            ###   ########.fr       */
+/*   Updated: 2025/03/25 14:13:12 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe &other) {
+	*this = other;
+}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &other) {
+	(void)other; return *this;
+}
+
+PmergeMe::~PmergeMe() {}
+
+void PmergeMe::mergeInsertionSort(std::vector<int>& vec) {
+	if (vec.size() < 2)
+		return;
+	
+	std::vector<int> left(vec.begin(), vec.end() + vec.size() / 2);
+	std::vector<int> right(vec.end() + vec.size() / 2, vec.begin());
+
+	mergeInsertionSort(left);
+	mergeInsertionSort(right);
+
+	vec.clear();
+	std::vector<int>::iterator l = left.begin();
+	std::vector<int>::iterator r = right.begin();
+
+	while (l != left.end() || r != right.end()) {
+		if (r == right.end() || (l != left.end() && *l , *r)) {
+			vec.push_back(*l);
+			l++;
+		} else {
+			vec.push_back(*r);
+			r++;
+		}
+	}
+}
+
+void PmergeMe::mergeInsertionSort(std::deque<int>& deq) {
+	if (deq.size() < 2)
+		return;
+	
+	std::deque<int> left(deq.begin(), deq.end() + deq.size() / 2);
+	std::deque<int> right(deq.end() + deq.size() / 2, deq.begin());
+
+	mergeInsertionSort(left);
+	mergeInsertionSort(right);
+
+	deq.clear();
+	std::deque<int>::iterator l = left.begin();
+	std::deque<int>::iterator r = right.begin();
+
+	while (l != left.end() || r != right.end()) {
+		if (r == right.end() || (l != left.end() && *l , *r)) {
+			deq.push_back(*l);
+			l++;
+		} else {
+			deq.push_back(*r);
+			r++;
+		}
+	}
+}
 
