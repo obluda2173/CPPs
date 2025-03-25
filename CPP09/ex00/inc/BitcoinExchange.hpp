878b813208ae787a49583d7cf2d1d45d9fb8a966
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-03-25 11:21:34 +0100

    beauty changes

--- CPP09/ex00/inc/BitcoinExchange.hpp
+++ CPP09/ex00/inc/BitcoinExchange.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: erian <erian@student.42>                   +#+  +:+       +#+        */
+/*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 12:59:16 by erian             #+#    #+#             */
-/*   Updated: 2025/03/23 17:10:59 by erian            ###   ########.fr       */
+/*   Updated: 2025/03/25 11:06:19 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,23 @@
 
 class BitcoinExchange {
 	private:
+		// attributes
 		std::map<std::string, double> exchangeRates;
 	
+		// methods
 		bool isValidDate(const std::string& date) const;
 		bool isValidValue(const std::string& value) const;
 		std::string trim(const std::string& str) const;
 		std::string getClosestDate(const std::string& date) const;
 
 	public:
+		// OCF
 		BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange& other);
 		BitcoinExchange& operator=(const BitcoinExchange& other);
 		~BitcoinExchange();
 
+		// methods
 		bool loadExchangeRates(const std::string& filename);
 		void processInputFile(const std::string& filename) const;
 };
--- CPP09/ex01/src/_main.cpp
+++ CPP09/ex01/src/_main.cpp
@@ -3,28 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   _main.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: erian <erian@student.42>                   +#+  +:+       +#+        */
+/*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 13:24:03 by erian             #+#    #+#             */
-/*   Updated: 2025/03/24 19:37:12 by erian            ###   ########.fr       */
+/*   Updated: 2025/03/25 11:17:03 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
 int main(int ac, char **av) {
-    if (ac != 2) {
-        std::cerr << "Error: Invalid number of arguments" << std::endl;
-        return 1;
-    }
+	if (ac != 2) {
+		std::cerr << "Error: Invalid number of arguments" << std::endl;
+		return 1;
+	}
 
-    try {
-        int result = evaluate(av[1]);
-        std::cout << BLUE << result << NC << std::endl;
-    } catch (const std::exception &e) {
-        std::cerr << ERROR << e.what() << NC << std::endl;
-        return 1;
-    }
+	try {
+		int result = evaluate(av[1]);
+		std::cout << BLUE << result << NC << std::endl;
+	} catch (const std::exception &e) {
+		std::cerr << ERROR << e.what() << NC << std::endl;
+		return 1;
+	}
 
-    return 0;
+	return 0;
 }
